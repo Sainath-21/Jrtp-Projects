c63fef13aaa18d990262005526cae6f59386d8eb
Sainath-21 <sainath2580rb@gmail.com>
2025-04-06 14:17:52 +0530

    Dev Commited A.java File

---
+++ Java_Project_01/src/com/nt/java/A.java
@@ -0,0 +1,16 @@
+package com.nt.java;
+
+public class A {
+
+	public static void main(String[] args) {
+
+		
+		
+	System.out.println("HIIIIIII");
+	System.out.println("Hello");
+	System.out.println("HRU?");
+	System.out.println("I am Fine........");
+
+	}
+
+}
